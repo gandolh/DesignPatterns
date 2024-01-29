b362e4ef8d6681d3a4a0738882270f5957b19290
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-01-29 19:08:45 +0200

    Added abstract factory

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿using DesignPatterns.Factory;
+﻿
+#region factory pattern
+//using DesignPatterns.Factory;
 
-FactoryMain factoryMain = new FactoryMain(OsType.Windows);
-factoryMain.main();
+// factory pattern
+// os type should be got by configuration / system environment
+//FactoryMain factoryMain = new FactoryMain(OsType.Windows);
+//factoryMain.main();
+
+// abstract factory
+#endregion
+
+#region abstract factory pattern
+using DesignPatterns.AbstractFactory;
+
+
+// the abstract factory is the gui factory.
+AbstractFactoryMain abstractFactoryMain = new AbstractFactoryMain(OsType.Mac);
+abstractFactoryMain.Main();
+#endregion
