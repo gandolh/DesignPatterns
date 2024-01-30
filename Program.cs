f0b364a2c2bff7beef39dd70524c7c0e376a9c04
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-01-30 18:08:09 +0200

    implemented singleton pattern

--- Program.cs
+++ Program.cs
@@ -28,11 +28,17 @@
 
 #region Prototype pattern
 
-using DesignPatterns.Prototype;
-// There exists an interface (prototype) that has method: Clone.
-// The clone method uses a copy constructor (or clone in clone method) and creates
-// a new object that is copy of current object
-PrototypeMain prototypeMain = new PrototypeMain();
-prototypeMain.Main();
+//using DesignPatterns.Prototype;
+//// There exists an interface (prototype) that has method: Clone.
+//// The clone method uses a copy constructor (or clone in clone method) and creates
+//// a new object that is copy of current object
+//PrototypeMain prototypeMain = new PrototypeMain();
+//prototypeMain.Main();
 
+#endregion
+
+#region Singleton pattern
+using DesignPatterns.Singleton;
+SingletonMain singletonMain = new SingletonMain();
+singletonMain.Main();
 #endregion
